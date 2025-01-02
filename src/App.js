c186be1cbdd3e93561c8c97950a8a543160eea77
Emily Pondaven <emily.pondaven@gmail.com>
2025-01-02 21:38:06 +0000

    Main to-do list screen

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-main">
         <nav>
           <a href="daily">Daily</a>
           <a href="main">Main</a>
         </nav>
-        <h1>Quest Log</h1>
-        <div>
-          <input placeholder="Enter task here"></input>
-          <button>Add</button>
+        <h1>Daily To-do List</h1>
+
+        <div class="task-field">
+          <input class="task-input" placeholder="Enter task here"></input>
+          <button class="add-button">Add</button>
         </div>
+
+        <li>
+          <ul>Spanish Duolingo Lesson</ul>
+          <ul>Coding project (GitHub repo push request)</ul>
+          <ul>Game of Chess</ul>
+          <ul>Reading book (Philosophy, Fiction)</ul>
+        </li>
       </header>
     </div>
   );
