db02395f1ec1c0fc908049e1d3291e685c341a68
Emily Pondaven <emily.pondaven@gmail.com>
2025-01-06 17:40:05 +0000

    organising

--- frontend/src/pages/Daily.js
+++ frontend/src/pages/Daily.js
@@ -4,6 +4,7 @@ import TaskList from '../components/TaskList';
 // Colour scheme: https://coolors.co/fff5e8-f76f8e-96616b-2a3e47-113537
 function Daily() {
     const [dailyTasks, setDailyTasks] = useState([]);
+    
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/daily`)
             .then((response) => response.json())
